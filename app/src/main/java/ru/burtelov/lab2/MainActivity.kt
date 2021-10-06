7a2f77ebc887b8fe4a5d184e7d05b4e92bc66cbb
vburtelov <vburtelov@gmail.com>
2021-10-06 21:23:58 +0300

    done

---
+++ app/src/main/java/ru/burtelov/lab2/MainActivity.kt
@@ -0,0 +1,13 @@
+package ru.burtelov.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
